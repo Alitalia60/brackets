2598633dab0c913aedea8c6850767c39b2279bea
Andrew Litvinov <lita.study@bk.ru>
2022-01-27 17:47:57 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-module.exports = function check(str, bracketsConfig) {
+// module.exports = function check(str, bracketsConfig) {
+function check(str, bracketsConfig) {
   // your solution
+
+  // let openingBrackets=[]
+  // let closingBrackets=[]
+  let pairCounter = {};
+
+  bracketsConfig.forEach((config) => {
+    
+    openBracketCounter[config[0]] = 0;
+    closeBracketCounter[config[1]] = 0;
+  });
+
+  for (let index = 0; index < str.length; index++) {
+    if (openBracketCounter[str[index]]) {
+      openBracketCounter[str[index]]++;
+    }
+    if (openBracketCounter[str[index]] < 0) {
+      console.log("error on index = ", index);
+    }
+    if (closeBracketCounter[str[index]]) {
+      closeBracketCounter[str[index]]++;
+    }
+  }
 }
+
+// check('()', [['(', ')']])
+const str = "()";
+const config = [["(", ")"]];
+
+check(str, config);
